f5ad8a49173f53707db1ce46c0d11efb95375ae7
Vu Anh <anhv.ict91@gmail.com>
2018-09-13 10:58:12 +0700

    update content

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 name="technique_report"
-name="2"
+if [ $# -eq 1 ]; then
+    name=$1
+fi
 inputfile=util/$name.tex
 output="tmp"
 pdflatex -file-line-error -interaction=nonstopmode -synctex=1 -output-format=pdf -output-directory=$output $inputfile
