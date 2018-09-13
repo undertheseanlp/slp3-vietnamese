5f72f30337e83357997d97f207bfdd6b6fa9d1a3
Vu Anh <anhv.ict91@gmail.com>
2018-09-13 09:36:11 +0700

    update content

--- build.sh
+++ build.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 name="technique_report"
-pdflatex -file-line-error -interaction=nonstopmode -synctex=1 -output-format=pdf -output-directory=. $name.tex
+name="2"
+inputfile=util/$name.tex
+output="tmp"
+pdflatex -file-line-error -interaction=nonstopmode -synctex=1 -output-format=pdf -output-directory=$output $inputfile
 bibtex $name.aux
-pdflatex -file-line-error -interaction=nonstopmode -synctex=1 -output-format=pdf -output-directory=. $name.tex
-pdflatex -file-line-error -interaction=nonstopmode -synctex=1 -output-format=pdf -output-directory=. $name.tex
+pdflatex -file-line-error -interaction=nonstopmode -synctex=1 -output-format=pdf -output-directory=$output $inputfile
+pdflatex -file-line-error -interaction=nonstopmode -synctex=1 -output-format=pdf -output-directory=$output $inputfile
 
-rm -rf $name.blg
-rm -rf $name.log
-rm -rf $name.out
-rm -rf *.aux
-rm -rf $name.bbl
-rm -rf $name.synctex.gz
+rm -rf $output/$name.blg
+rm -rf $output/$name.log
+rm -rf $output/$name.out
+rm -rf $output/*.aux
+rm -rf $output/$name.bbl
+rm -rf $output/$name.synctex.gz
