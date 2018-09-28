7d5ea87e2cee3eddcbc862485eaf470a0372b99a
Vu Anh <anhv.ict91@gmail.com>
2018-09-28 18:10:33 +0700

    update guildline

--- build.sh
+++ build.sh
@@ -1,18 +1,9 @@
 #!/usr/bin/env bash
-name="technique_report"
+name="1"
 if [ $# -eq 1 ]; then
     name=$1
 fi
 inputfile=util/$name.tex
 output="tmp"
 pdflatex -file-line-error -interaction=nonstopmode -synctex=1 -output-format=pdf -output-directory=$output $inputfile
-bibtex $name.aux
-pdflatex -file-line-error -interaction=nonstopmode -synctex=1 -output-format=pdf -output-directory=$output $inputfile
-pdflatex -file-line-error -interaction=nonstopmode -synctex=1 -output-format=pdf -output-directory=$output $inputfile
-
-rm -rf $output/$name.blg
-rm -rf $output/$name.log
-rm -rf $output/$name.out
-rm -rf $output/*.aux
-rm -rf $output/$name.bbl
-rm -rf $output/$name.synctex.gz
+bibtex $output/$name.aux
