2bfd538f022cf6dbc4aba419ea4bef46dbb009e3
anhv <anhv.ict91@gmail.com>
2018-09-29 00:11:59 +0700

    edit after alpha review

--- build.sh
+++ build.sh
@@ -6,4 +6,13 @@ fi
 inputfile=util/$name.tex
 output="tmp"
 pdflatex -file-line-error -interaction=nonstopmode -synctex=1 -output-format=pdf -output-directory=$output $inputfile
-bibtex $output/$name.aux
+bibtex $name.aux
+pdflatex -file-line-error -interaction=nonstopmode -synctex=1 -output-format=pdf -output-directory=$output $inputfile
+pdflatex -file-line-error -interaction=nonstopmode -synctex=1 -output-format=pdf -output-directory=$output $inputfile
+
+rm -rf $output/$name.blg
+rm -rf $output/$name.log
+rm -rf $output/$name.out
+rm -rf $output/*.aux
+rm -rf $output/$name.bbl
+rm -rf $output/$name.synctex.gz
