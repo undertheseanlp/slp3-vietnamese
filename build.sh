2814bdd564e6ba27ac223711c07225e52f429625
anhv <anhv.ict91@gmail.com>
2018-09-29 12:45:36 +0700

    add citations

--- build.sh
+++ build.sh
@@ -1,18 +1,41 @@
-#!/usr/bin/env bash
 name="1"
 if [ $# -eq 1 ]; then
     name=$1
 fi
-inputfile=util/$name.tex
-output="tmp"
+
+cp util/styles/* -t tmp
+cp util/layout/* -t tmp
+cp tex/$name.tex tmp/content.tex
+cp tex/$name.bib tmp/mybib.bib
+
+
+cd tmp
+output="."
+tmpfile="main"
+inputfile="main.tex"
 pdflatex -file-line-error -interaction=nonstopmode -synctex=1 -output-format=pdf -output-directory=$output $inputfile
-bibtex tmp/2.aux
+bibtex main.aux
+
 pdflatex -file-line-error -interaction=nonstopmode -synctex=1 -output-format=pdf -output-directory=$output $inputfile
+bibtex main.aux
 pdflatex -file-line-error -interaction=nonstopmode -synctex=1 -output-format=pdf -output-directory=$output $inputfile
 
-rm -rf $output/$name.blg
-rm -rf $output/$name.log
-rm -rf $output/$name.out
-rm -rf $output/*.aux
-rm -rf $output/$name.bbl
-rm -rf $output/$name.synctex.gz
+mv main.pdf $name.pdf
+
+rm -rf $tmpfile.blg
+rm -rf $tmpfile.log
+rm -rf $name.out
+rm -rf *.aux
+rm -rf $tmpfile.bbl
+rm -rf $tmpfile.synctex.gz
+
+rm -rf content.tex
+rm -rf mybib.bib
+rm -rf begin.tex
+rm -rf end.tex
+rm -rf main.tex
+
+
+# Remove styles
+rm acl_natbib.bst
+rm undertheseabook.sty
